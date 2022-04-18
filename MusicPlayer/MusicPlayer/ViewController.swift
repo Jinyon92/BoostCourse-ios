f28d44adb0280f3be6cadee9e7a321095780bef1
choijinhyun <chjh0902@naver.com>
2022-04-18 16:03:30 +0900

    init

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 최진현 on 2022/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
